1a8d44fc2b5c8dcfebecdac55518e0a38b7850f8
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-11-10 14:52:54 +0530

    applied rate limiting on all the routes for preventing brute-force attacks

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import authRoutes from './routes/authRoutes';
+import userRoutes from './routes/userRoutes';
 import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
 
 const app: Application = express();
 
@@ -13,11 +15,20 @@ connectDB();
 app.use(express.json());
 app.use(morgan('dev'));
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again later.'
+});
+
+app.use(limiter);
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Evolv API is running...');
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 
 const port = process.env.PORT || 8080 || 5000 || 5173 || 10000 || 3100;
 
