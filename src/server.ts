c116dfff79d0b53746a0a7f979b3b4f7071b9765
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-12-16 19:55:47 +0530

    Integrate session management and cookie parsing in server setup
    - Added express-session and cookie-parser middleware for enhanced session handling and secure cookie management.
    - Initialized Passport for user authentication and session management.
    - Updated server configuration to streamline session handling and improve security.
    - Removed redundant port options for clarity.

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,15 @@ import userRoutes from "./routes/userRoutes"
 import habitRoutes from "./routes/habitRoutes"
 import morgan from "morgan"
 import rateLimit from "express-rate-limit"
+import passport from 'passport'
+import session from 'express-session'
+import './config/passport'
 import {
   dailyHabitReminder,
   weeklyHabitReminder,
   monthlyHabitReminder,
 } from "./crons/habitReminder"
+import cookieParser from 'cookie-parser'
 
 const app: Application = express()
 
@@ -28,8 +32,18 @@ const limiter = rateLimit({
 })
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(morgan("dev"))
 app.use(limiter)
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'your-secret-key',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Evolv API is running...")
@@ -39,7 +53,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
 app.use("/api/habits", habitRoutes)
 
-const port = process.env.PORT || 8080 || 5000 || 5173 || 10000 || 3100
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
