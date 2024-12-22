4cf5fa1cccf82919b6bb3c4c1def99ddfb4f53de
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-12-22 12:21:17 +0530

    Remove Facebook OAuth strategy from Passport configuration

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -2,7 +2,6 @@ import passport from "passport"
 import dotenv from "dotenv"
 import { Strategy as GoogleStrategy } from "passport-google-oauth20"
 import { Strategy as GithubStrategy } from "passport-github2"
-import { Strategy as FacebookStrategy } from "passport-facebook"
 import User from "../models/userSchema"
 
 dotenv.config()
@@ -115,69 +114,6 @@ passport.use(
           if (!existingOauth) {
             user.oauth_info!.push({
               provider: "github",
-              provider_id: profile.id,
-              access_token: accessToken,
-              refresh_token: refreshToken,
-              expires_at: new Date(Date.now() + 120 * 24 * 60 * 60 * 1000),
-            })
-            await user.save()
-          }
-        }
-        done(null, user)
-      } catch (error) {
-        done(error, null)
-      }
-    }
-  )
-)
-
-// Facebook OAuth Strategy
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_APP_ID as string,
-      clientSecret: process.env.FACEBOOK_APP_SECRET as string,
-      callbackURL: "/auth/facebook/callback",
-      profileFields: ['id', 'emails', 'name', 'picture.type(large)'],
-    },
-    async (
-      accessToken: string,
-      refreshToken: string,
-      profile: any,
-      done: (error: any, user?: any) => void
-    ) => {
-      try {
-        let user = await User.findOne({
-          "personal_info.email": profile.emails[0].value,
-        })
-
-        if (!user) {
-          user = new User({
-            personal_info: {
-              first_name: profile.name.givenName,
-              last_name: profile.name.familyName,
-              email: profile.emails[0].value,
-              profile_picture: profile.photos?.[0]?.value,
-            },
-            oauth_info: [
-              {
-                provider: "facebook",
-                provider_id: profile.id,
-                access_token: accessToken,
-                refresh_token: refreshToken,
-                expires_at: new Date(Date.now() + 120 * 24 * 60 * 60 * 1000),
-              },
-            ],
-          })
-          await user.save()
-        } else {
-          // Update existing user with Facebook provider info if not already present
-          const existingOauth = user.oauth_info?.find(
-            (oauth) => oauth.provider_id === profile.id
-          )
-          if (!existingOauth) {
-            user.oauth_info!.push({
-              provider: "facebook",
               provider_id: profile.id,
               access_token: accessToken,
               refresh_token: refreshToken,
